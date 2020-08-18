055d4f8caf854a05766c32dc3c84f024fea7096c
Samantha (Killin' it) Shepherd <samantha.shepherd@dubitlimited.com>
2020-08-18 09:31:15 +0100

    👓Refactor help screen

--- src/components/help/HelpScreen.tsx
+++ src/components/help/HelpScreen.tsx
@@ -1,55 +1,50 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 
+// Navigation
 import Routes from '../../routes/Routes';
-import MenuButton from '../menu/MenuButton';
-import Styles from './HelpStyles';
+// Components
+import MenuButton from '../shared/buttons/MenuButton';
+import ScreenWrapper from '../shared/ScreenWrapper';
 
-const HelpScreen = () => {
-  return (
-    <View style={Styles.flexColumn}>
-      <View style={Styles.avatarContainer}>
-        <Text style={Styles.nameText}>Help</Text>
-      </View>
-      <View style={Styles.buttonContainer}>
-        <MenuButton
-          text="Buy Guide"
-          route={Routes.HELP_INFO}
-          params={{header: 'Buy Guide'}}
-        />
-        <MenuButton
-          text="General Information"
-          route={Routes.HELP_INFO}
-          params={{header: 'General Information'}}
-        />
-        <MenuButton
-          text="Payment"
-          route={Routes.HELP_INFO}
-          params={{header: 'Payment'}}
-        />
-        <MenuButton
-          text="Shipping"
-          route={Routes.HELP_INFO}
-          params={{header: 'Shipping'}}
-        />
-        <MenuButton
-          text="Return"
-          route={Routes.HELP_INFO}
-          params={{header: 'Return'}}
-        />
-        <MenuButton
-          text="Exchanges"
-          route={Routes.HELP_INFO}
-          params={{header: 'Exchanges'}}
-        />
-        <MenuButton
-          text="Recalled Items"
-          route={Routes.HELP_INFO}
-          params={{header: 'Recalled Items'}}
-        />
-      </View>
-    </View>
-  );
-};
-
+const HelpScreen = () => (
+  <ScreenWrapper header="Help" scroll>
+    <>
+      <MenuButton
+        text="Buy Guide"
+        route={Routes.HELP_INFO}
+        params={{header: 'Buy Guide'}}
+      />
+      <MenuButton
+        text="General Information"
+        route={Routes.HELP_INFO}
+        params={{header: 'General Information'}}
+      />
+      <MenuButton
+        text="Payment"
+        route={Routes.HELP_INFO}
+        params={{header: 'Payment'}}
+      />
+      <MenuButton
+        text="Shipping"
+        route={Routes.HELP_INFO}
+        params={{header: 'Shipping'}}
+      />
+      <MenuButton
+        text="Return"
+        route={Routes.HELP_INFO}
+        params={{header: 'Return'}}
+      />
+      <MenuButton
+        text="Exchanges"
+        route={Routes.HELP_INFO}
+        params={{header: 'Exchanges'}}
+      />
+      <MenuButton
+        text="Recalled Items"
+        route={Routes.HELP_INFO}
+        params={{header: 'Recalled Items'}}
+      />
+    </>
+  </ScreenWrapper>
+);
 export default HelpScreen;
